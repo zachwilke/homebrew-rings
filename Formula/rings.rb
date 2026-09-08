8db80b602574cc6c80288d6edd2dc72cc072cc94
zachwilke <45574453+zachwilke@users.noreply.github.com>
2026-09-08 18:59:07 -0500

    Bump rings formula to 0.3.3

--- Formula/rings.rb
+++ Formula/rings.rb
@@ -1,7 +1,7 @@
 class Rings < Formula
   desc "DaisyDisk-style disk usage TUI"
   homepage "https://github.com/zachwilke/rings"
-  version "0.2.0"
+  version "0.3.3"
   license "MIT"
 
   livecheck do
@@ -11,23 +11,23 @@ class Rings < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/zachwilke/rings/releases/download/v0.2.0/rings-aarch64-apple-darwin.xz"
-      sha256 "744b88cca44b8e6ef405aca6c52e4e848c353551a609734fcb289554b4e156a4"
+      url "https://github.com/zachwilke/rings/releases/download/v0.3.3/rings-aarch64-apple-darwin.xz"
+      sha256 "2add131fcc484d89f0e883a2fdd0dcf565607026e77067235006e591151c109f"
     end
     on_intel do
-      url "https://github.com/zachwilke/rings/releases/download/v0.2.0/rings-x86_64-apple-darwin.xz"
-      sha256 "7f721ac1a8f7f1fd6998fdf165c927fc358cbccba70973856f5581bfad5a75b2"
+      url "https://github.com/zachwilke/rings/releases/download/v0.3.3/rings-x86_64-apple-darwin.xz"
+      sha256 "10ef785bdcf8da6846de99369ec04a2317100c24deff4872c4886ddf9415912c"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/zachwilke/rings/releases/download/v0.2.0/rings-x86_64-linux-musl.xz"
-      sha256 "b64c07fd6f3c3827646e016476d119e68188319cd939a3796f8605cf821ef4e1"
+      url "https://github.com/zachwilke/rings/releases/download/v0.3.3/rings-x86_64-linux-musl.xz"
+      sha256 "1987bb4948a61872a663b01506093587748d0eb4b5b4f86443b63ea2ed140490"
     end
     on_arm do
-      url "https://github.com/zachwilke/rings/releases/download/v0.2.0/rings-aarch64-linux-musl.xz"
-      sha256 "95647a952c37ce97e481cea5d76fedcd02a1f63b7ff62da6199691f66b25ed9e"
+      url "https://github.com/zachwilke/rings/releases/download/v0.3.3/rings-aarch64-linux-musl.xz"
+      sha256 "355a218d326dbe42a70d3fc7faf75d380df6d250c9c8f736c347fd2e9e03118e"
     end
   end
 
